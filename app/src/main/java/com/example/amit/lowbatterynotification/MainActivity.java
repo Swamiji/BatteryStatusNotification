ff7756ecb20b35aafab21cb3f0c8b8f5836eadc7
Amit Roy <amitroy314@gmail.com>
2017-07-22 14:21:44 +0530

    BatteryStatusNotification
    BatteryStatusNotification

---
+++ app/src/main/java/com/example/amit/lowbatterynotification/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.amit.lowbatterynotification;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
